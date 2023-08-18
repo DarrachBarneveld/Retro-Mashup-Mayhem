50b6ec172367449daa5089f4ab77b3a4d015df67
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-08-18 20:16:00 +0000

    #12 Add tile size

--- src/classes/Level.js
+++ src/classes/Level.js
@@ -1,12 +1,16 @@
 import K from "../kaboom";
 
 export default class Level {
-  constructor(data, objectsMapping) {
-    this.data = data;
-    this.objectsMapping = objectsMapping;
-  }
+    constructor(data, objectsMapping) {
+        this.data = data;
+        this.objectsMapping = objectsMapping;
+    }
 
-  loadLevel() {
-    K.addLevel(this.data, this.objectsMapping);
-  }
+    loadLevel() {
+        K.addLevel(this.data, {
+          tileWidth: 32,
+          tileHeight: 32, 
+            ...this.objectsMapping
+        });
+    }
 }

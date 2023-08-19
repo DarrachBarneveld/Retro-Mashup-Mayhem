f9d11260a67ebe412146e845db69272892595f9c
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-19 15:49:44 +0100

    Added enemy follow player movement

--- src/kaboom.js
+++ src/kaboom.js
@@ -2,7 +2,7 @@ import kaboom from "kaboom";
 
 export default K = kaboom({
   width: 340,
-  height: 260,
+  height: 280,
   scale: 2,
   background: [0, 0, 0, 0.0],
   canvas: document.querySelector("#mycanvas"),

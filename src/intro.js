3c992789b436c7f347a55828ea55ec322ad9815c
Daniel Morriss <danielmorriss1@gmail.com>
2023-08-19 21:29:27 +0100

    Add title to intro.html

--- src/intro.js
+++ src/intro.js
@@ -1,8 +1,8 @@
 /*Change Page after Scroll animation */
 const titles = document.querySelector('#titlecontent');
-const duration = 50000; // Duration of the scroll animation in milliseconds
+const duration = 30500; // Duration of the scroll animation in milliseconds
 
 setTimeout(() => {
-  window.location.href = 'intro.html';
+  window.location.href = 'index.html';
 }, duration);
 

02e08754ffc5d32882b4f1821cabbd1b1b8bed10
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-19 20:12:07 +0100

    Fixed merge errors

--- src/classes/Level.js
+++ src/classes/Level.js
@@ -65,9 +65,8 @@ export class Level2 {
     K.loadSprite("tiles", marioTileset, { sliceX: 8, sliceY: 8 });
     K.loadSprite("mario", mario, { sliceX: 1, sliceY: 2 });
     K.loadSprite("cloud", cloud);
-    K.loadSprite("pipe", pipe, { sliceX: 1, sliceY: 2 }); 
-    K.loadSprite("castle", castle, { sliceX: 4, sliceY: 4 }); 
-
+    K.loadSprite("pipe", pipe, { sliceX: 1, sliceY: 2 });
+    K.loadSprite("castle", castle, { sliceX: 4, sliceY: 4 });
 
     K.addLevel(level2, {
       tileWidth: 16,
@@ -90,24 +89,12 @@ export class Level2 {
         ],
 
         // Small bush
-        "a": () => [
-          K.sprite("tiles", { frame: 48 }),
-          "tiles",
-        ],
-        "b": () => [
-          K.sprite("tiles", { frame: 49 }),
-          "tiles",
-        ],
-        "c": () => [
-          K.sprite("tiles", { frame: 50 }),
-          "tiles",
-        ],
+        a: () => [K.sprite("tiles", { frame: 48 }), "tiles"],
+        b: () => [K.sprite("tiles", { frame: 49 }), "tiles"],
+        c: () => [K.sprite("tiles", { frame: 50 }), "tiles"],
 
         // Clouds - to add the whole cloud you need to put out ^^ in the layout
-        "^": () => [
-          K.sprite("cloud"),
-          "cloud",
-        ],
+        "^": () => [K.sprite("cloud"), "cloud"],
 
         // Super Mario
 
@@ -128,7 +115,6 @@ export class Level2 {
         ],
 
         // Pipe
-        "x": () => [
         x: () => [
           K.sprite("pipe", { frame: 0 }), // top half
           K.area(),
@@ -143,25 +129,25 @@ export class Level2 {
         ],
 
         // Castle
-        "A": () => [K.sprite("castle", { frame: 0 })],  // Top-left corner
-        "B": () => [K.sprite("castle", { frame: 1 })],  // Top-second from left
-        "C": () => [K.sprite("castle", { frame: 2 })],  // Top-third from left
-        "D": () => [K.sprite("castle", { frame: 3 })],  // Top-right corner
+        A: () => [K.sprite("castle", { frame: 0 })], // Top-left corner
+        B: () => [K.sprite("castle", { frame: 1 })], // Top-second from left
+        C: () => [K.sprite("castle", { frame: 2 })], // Top-third from left
+        D: () => [K.sprite("castle", { frame: 3 })], // Top-right corner
 
-        "E": () => [K.sprite("castle", { frame: 4 })],  // Second row - left
-        "F": () => [K.sprite("castle", { frame: 5 })],  // Second row - second from left
-        "G": () => [K.sprite("castle", { frame: 6 })],  // Second row - third from left
-        "H": () => [K.sprite("castle", { frame: 7 })],  // Second row - right
+        E: () => [K.sprite("castle", { frame: 4 })], // Second row - left
+        F: () => [K.sprite("castle", { frame: 5 })], // Second row - second from left
+        G: () => [K.sprite("castle", { frame: 6 })], // Second row - third from left
+        H: () => [K.sprite("castle", { frame: 7 })], // Second row - right
 
-        "I": () => [K.sprite("castle", { frame: 8 })],  // Third row - left
-        "J": () => [K.sprite("castle", { frame: 9 })],  // Third row - second from left
-        "K": () => [K.sprite("castle", { frame: 10 })], // Third row - third from left
-        "L": () => [K.sprite("castle", { frame: 11 })], // Third row - right
+        I: () => [K.sprite("castle", { frame: 8 })], // Third row - left
+        J: () => [K.sprite("castle", { frame: 9 })], // Third row - second from left
+        K: () => [K.sprite("castle", { frame: 10 })], // Third row - third from left
+        L: () => [K.sprite("castle", { frame: 11 })], // Third row - right
 
-        "M": () => [K.sprite("castle", { frame: 12 })], // Bottom row - left
-        "N": () => [K.sprite("castle", { frame: 13 })], // Bottom row - second from left
-        "O": () => [K.sprite("castle", { frame: 14 })], // Bottom row - third from left
-        "P": () => [K.sprite("castle", { frame: 15 })], // Bottom row - right
+        M: () => [K.sprite("castle", { frame: 12 })], // Bottom row - left
+        N: () => [K.sprite("castle", { frame: 13 })], // Bottom row - second from left
+        O: () => [K.sprite("castle", { frame: 14 })], // Bottom row - third from left
+        P: () => [K.sprite("castle", { frame: 15 })], // Bottom row - right
       },
     });
   }

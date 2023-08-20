415a7c7b787f17401635b8c1aa39eeb9560ddc2f
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-20 20:43:30 +0100

    Added import filepath for kaboom

--- src/kaboom.js
+++ src/kaboom.js
@@ -1,4 +1,5 @@
 import kaboom from "kaboom";
+const canvas = document.getElementById("mycanvas");
 
 console.log(kaboom);
 export default K = kaboom({
@@ -6,5 +7,5 @@ export default K = kaboom({
   height: 272,
   scale: 2,
   background: [0, 0, 0, 0.0],
-  canvas: document.querySelector("#mycanvas"),
+  canvas,
 });

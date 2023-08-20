341bba0e5b092411227e1f57c290b90cfc1393e5
Daniel Morriss <danielmorriss1@gmail.com>
2023-08-20 08:58:31 +0100

    Start adding js to pplay sounnd on intro-tv

--- src/intro.js
+++ src/intro.js
@@ -5,3 +5,18 @@ const duration = 305000; // Duration of the scroll animation in milliseconds
 setTimeout(() => {
   window.location.href = 'index.html';
 }, duration);
+
+/* Sound toggle */
+
+const soundOnButton = document.getElementById('sound-on');
+const soundOffButton = document.getElementById('sound-off');
+
+soundOffButton.addEventListener('click', () => {
+  soundOffButton.classList.remove('hide');
+  soundOnButton.classList.add('hide');
+});
+
+soundOnButton.addEventListener('click', () => {
+  soundOnButton.classList.remove('hide');
+  soundOffButton.classList.add('hide');
+});

c7bcce8b788213468fe450ed997642fbc2470f4f
Daniel Morriss <danielmorriss1@gmail.com>
2023-08-20 08:14:38 +0100

    Add intro-tv.html to try intro inside the TV

--- src/intro.js
+++ src/intro.js
@@ -1,6 +1,6 @@
 /*Change Page after Scroll animation */
 const titles = document.querySelector('#titlecontent');
-const duration = 30500; // Duration of the scroll animation in milliseconds
+const duration = 305000; // Duration of the scroll animation in milliseconds
 
 setTimeout(() => {
   window.location.href = 'index.html';

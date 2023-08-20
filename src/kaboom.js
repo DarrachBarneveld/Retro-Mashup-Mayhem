3ecf7da02055f7eaf5edafc77ce9055192597b6f
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-20 20:46:13 +0100

    Added constant variable to kaboom instance

--- src/kaboom.js
+++ src/kaboom.js
@@ -1,11 +1,11 @@
 import kaboom from "kaboom";
-const canvas = document.getElementById("mycanvas");
 
-console.log(kaboom);
-export default K = kaboom({
+const K = kaboom({
   width: 341,
   height: 272,
   scale: 2,
   background: [0, 0, 0, 0.0],
-  canvas,
+  canvas: document.getElementById("mycanvas"),
 });
+
+export default K;

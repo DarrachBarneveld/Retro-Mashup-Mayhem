ad3ccc7d8780f02be54ad4e189ab2bc29c0a75b7
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-21 13:56:44 +0100

    Added file changes to run intro at start

--- src/intro.js
+++ src/intro.js
@@ -1,26 +1,25 @@
 /*Change Page after Scroll animation */
-const titles = document.querySelector('#titlecontent');
+const titles = document.querySelector("#titlecontent");
 const duration = 22550; // Duration of the scroll animation in milliseconds
 
 setTimeout(() => {
-  window.location.href ="index.html";
+  window.location.href = "menu.html";
 }, duration);
 
 /* Sound toggle */
 
-const soundOnButton = document.getElementById('sound-on');
-const soundOffButton = document.getElementById('sound-off');
-const backgroundMusic = document.getElementById('backgroundMusic');
+const soundOnButton = document.getElementById("sound-on");
+const soundOffButton = document.getElementById("sound-off");
+const backgroundMusic = document.getElementById("backgroundMusic");
 
-soundOffButton.addEventListener('click', () => {
-  soundOffButton.classList.add('hide');
-  soundOnButton.classList.remove('hide');
+soundOffButton.addEventListener("click", () => {
+  soundOffButton.classList.add("hide");
+  soundOnButton.classList.remove("hide");
   backgroundMusic.play();
-
 });
 
-soundOnButton.addEventListener('click', () => {
-  soundOnButton.classList.add('hide');
-  soundOffButton.classList.remove('hide');
+soundOnButton.addEventListener("click", () => {
+  soundOnButton.classList.add("hide");
+  soundOffButton.classList.remove("hide");
   backgroundMusic.pause();
 });
